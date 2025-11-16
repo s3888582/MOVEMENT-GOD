6a1d3a2773174393917b109056b420539d724803
s3888582 <s388852@student.rmit.edu.au>
2025-11-16 18:03:29 +1100

    cleaned up hierarchy and scene, also added test ramps

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public Camera playerCamera;
     public float walkSpeed = 6f;
     public float runSpeed = 12f;
-    public float jumpPower = 7f;
+    public float jumpPower = 3f;
     public float gravity = 10f;
     public float lookSpeed = 2f;
     public float lookXLimit = 45f;
